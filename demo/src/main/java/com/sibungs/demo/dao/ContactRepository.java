be6514ee310832db1ab1a9dbdb99c3394a568d0c
Surem19 <dloper02@gmail.com>
2020-02-17 13:06:14 +0100

    Update ContactRepository.java

--- demo/src/main/java/com/sibungs/demo/dao/ContactRepository.java
+++ demo/src/main/java/com/sibungs/demo/dao/ContactRepository.java
@@ -3,6 +3,6 @@ package com.sibungs.demo.dao;
 import com.sibungs.demo.dto.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ContactRepository extends JpaRepository<Contact, Long> {
+public interface ContactRepository   extends JpaRepository<Contact, Long> {
 
 }
